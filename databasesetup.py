ec4231d769365fe1eb30dc9d5587dceef6e6b05e
zr123 <cGyLWLCcxhc4spbcYMqR>
2018-03-19 14:38:59 +0100

    fresh database and sanitychecked initial setup values

--- databasesetup.py
+++ databasesetup.py
@@ -24,7 +24,7 @@ mehlsack.save()
 
 # create RecipeIngredients
 RecipeList(recipe=kuchen, ingredient=butter, amount=300).save()
-RecipeList(recipe=kuchen, ingredient=mehl, amount=300).save()
+RecipeList(recipe=kuchen, ingredient=mehl, amount=0.5).save()
 RecipeList(recipe=mehlsack, ingredient=mehl, amount=1000).save()
 
 # create SuperUser
